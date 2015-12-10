a3966580096ca83c246d95792c9fb18746204685
Jimmyharidy <jimmy.haridy@hotmail.com>
2015-12-10 09:35:32 +0100

    Kordinate solution added

---
+++ Struct/Kordinat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    // En struct är en egendefinierad värdetyp
+    // har inga referenser till heapen
+    // är inmplicit sealed
+    // är public per default
+    // skrivs som vi skriver en Klass, men har nyckelordet struct
+    // alla de primitiva typerna är structar,
+    // ex, bool, int, long, double, float, char, DateTime
+    // ungefär 3ggr snabbare än en Class om 5 fält finns
+    // kan inte vara null, pga kan inte referera till heapen
+    struct Kordinat
+    {
+        public int X;
+        public int Y;
+        public string Z;
+
+       
+       //public int Z = 9;  // Does not compute, vi får inte sätta värde på fält i en struct
+        public override string ToString() => $"X: {X}, Y: {Y}, Z: {Z}";
+
+    }
+
+    //struct structXYZ : Kordinat
+    //{
+    //    // Kan och får inte ärva från en struct
+    //}
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StructDemo1();
+        }
+
+        private static void StructDemo1()
+        {
+            Kordinat kordinat;
+            kordinat.X = 66;
+            kordinat.Y = 88;
+
+            Kordinat origo = new Kordinat() { X = 50, Y = 50 };
+            origo.Z = "19";
+            origo = new Kordinat();  // Nollställer structen
+            // alla fält får defaultvärdet från respektive datatyp
+            // observera att string fick null i sig nu. 
+            Console.WriteLine(origo);
+            Console.ReadLine();
+        }
+    }
+}
