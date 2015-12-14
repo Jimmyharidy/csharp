a19292e6cf53643a91e52021a2ba9442eaa6d359
Jimmyharidy <jimmy.haridy@hotmail.com>
2015-12-14 13:48:14 +0100

    Added Icomparable

---
+++ IComparableNogonting/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparableNogonting
+{
+    class MyClass: IComparable<MyClass>
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private string Gender { get; set; }
+
+        public MyClass(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public int CompareTo(MyClass other)
+        {
+            return Age.CompareTo(other.Age);
+        }
+
+        public override string ToString() => $"Name: {Name}, Age: {Age}, Gender: {Gender}";
+       
+    }
+}
---
+++ IComparableNogonting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparableNogonting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            var lista = new List<MyClass>();
+            lista.Add(new MyClass("Jimmy", 31, "Male"));
+            lista.Add(new MyClass("Jimbo", 87, "Male"));
+            lista.Add(new MyClass("Jonna", 15, "Female"));
+            lista.Add(new MyClass("Johanna", 1, "Female"));
+            lista.Add(new MyClass("Olivia", 28, "Female"));
+            lista.Sort();
+            lista.ForEach(Console.WriteLine);
+
+
+            // Skapa en POCO - Klass med tre attribute
+            // Lägg 5 av klassen i en kollektion
+            // Använd sort() på kollektionen
+        }
+    }
+}
