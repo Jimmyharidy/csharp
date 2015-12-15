7730cd02b6aa06d98aa3cbe5a4d421744377eaee
Jimmyharidy <jimmy.haridy@hotmail.com>
2015-12-15 15:37:52 +0100

    TestTenta added

---
+++ TestTenta/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTenta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Teachers teacher1 = new Teachers() {Name = "Jimmy", Position = "Helsingborg"};
+            Teachers teacher2 = new Teachers();
+            List<Teachers> teacherList = new List<Teachers>();
+            teacherList.Add(teacher1);
+            teacherList.Add(teacher2);
+            teacherList.Sort();
+            Console.WriteLine(teacherList);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestTenta/Teachers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTenta
+{
+    public class Teachers: IComparable<Teachers>
+    {
+       private string name;
+      private string position;
+
+        //public string Name
+        //{
+        //    get {return name;}
+        //    set { name = value;}
+        //}
+
+        //public string Position
+        //{
+        //    get {return position ; }
+        //    set { position = value; }
+        //}
+
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public Teachers()
+        {
+            Name = "Adolf";
+            Position = "Tyskland";
+        }
+
+        public Teachers(string name, string position)
+        {
+            Name = name;
+            Position = position;
+        }
+
+        public int CompareTo(Teachers obj)
+        {
+            return obj.Name.CompareTo(this.Name);
+        }
+
+        public override string ToString() => $"Name: {Name}\r\nPosition: {Position}";
+    }
+
+}
