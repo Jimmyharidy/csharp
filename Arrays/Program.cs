ff8345f8c9b36374e67fb1fc841e02508fad244f
Jimmyharidy <jimmy.haridy@hotmail.com>
2015-12-17 20:17:45 +0100

    Arrays added to the solution

---
+++ Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[6];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i*20;
+                Console.WriteLine("Strange value {0} = {1}", i, myArray[i]);
+            }
+            Console.ReadLine();
+
+
+        }
+        
+    } 
+}
