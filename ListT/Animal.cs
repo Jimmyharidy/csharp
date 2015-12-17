fb437b26cdd99d96e7c3cdfbe76d550598a2c181
Jimmyharidy <jimmy.haridy@hotmail.com>
2015-12-17 15:31:44 +0100

    ListT added

---
+++ ListT/Animal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListT
+{
+    class Animal: IComparable<Animal>
+    {
+        private string kind;
+        private string name;
+        private int age;
+        public string Kind
+        {
+            get { return kind; }
+            set { kind = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Animal()
+        {
+            Kind = "Lion";
+            Name = "Zimba";
+            Age = 7;
+        }
+
+        public Animal(string kind, string name, int age)
+        {
+            Kind = kind;
+            Name = name;
+            Age = age;
+        }
+
+        public int CompareTo(Animal other)
+        {
+            return Age.CompareTo(other.age);
+        }
+
+        public override string ToString() => $"Kind: {Kind}\nName: {Name}\nAge: {Age}";
+    }
+}
---
+++ ListT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingLists();
+
+        }
+
+        private static void SortingLists()
+        {
+            var lista = new List<Animal>();
+            lista.Add(new Animal());
+            lista.Add(new Animal("Tiger", "Tigris", 5));
+            lista.Add(new Animal("Jaguar", "Jagis", 2));
+            lista.Add(new Animal("Hund", "Vovvov", 3));
+            lista.Sort();
+            //lista.Reverse();
+            foreach (var items in lista)
+            {
+                Console.WriteLine(items);
+            }
+            Console.ReadLine();
+        }
+    }
+}
